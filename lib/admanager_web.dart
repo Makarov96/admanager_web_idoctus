c644ddde04ecb9305f759409e4464c48b796b542
Makarov96 <estev97@gmail.com>
2025-06-20 19:11:35 -0600

    feat: upgrade lints

--- lib/admanager_web.dart
+++ lib/admanager_web.dart
@@ -1,8 +1,3 @@
-library admanager_web;
-
-/// Simple plugin to display ads from AdManager in Flutter Web
-/// This was primarily created to display Adsense Ads on Flutter Web,
-/// but it should work with any AdManager Ad.
 export 'src/adblock/adblock.dart' show AdBlock;
 export 'src/models/models.dart' show AdInfo;
 export 'src/adblock/adblock_size.dart' show AdBlockSize;
